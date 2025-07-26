4a101f726d28c6001a1b8811830935ee5fd4521f
soniya devikar <soniyadevikar6@gmail.com>
2025-07-26 12:34:26 +0530

    updated resume link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
                     <br></br>
                     <p className='download-resume'>
                         <a
-                            href="https://drive.google.com/file/d/1JKQwTXQMEfDcY01v_jJRPLAbq7DtQniT/view?usp=sharing"
+                            href="https://drive.google.com/file/d/19HMvqgS3ra0mdC7vQAwTGjFZK9ckV52H/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                             style={{
